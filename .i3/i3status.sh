c871c6c2de4f6e51269edb69cb6fe2dcd2f10810
Felix Hanley <felix@userspace.com.au>
2017-05-29 11:59:43 +1000

    Merge updates for X startup on OpenBSD laptop

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
 i3status -c ~/.i3/i3status.conf | (read line && echo $line && while :
 do
     read line
     dat=$(mpc current)
-    dat="[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" },"
-    echo "${line/\[/$dat}" || exit 1
+    if [ -z "${dat}" ]; then
+        echo "$line" || exit 1
+    else
+        dat="[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" },"
+        echo "${dat} ${line#\[}" || exit 1
+    fi
 done)
