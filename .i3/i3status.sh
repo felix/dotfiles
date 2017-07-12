e2e8655af55c76fb3401e7b9fe83566eae89bd9d
Felix Hanley <felix@userspace.com.au>
2017-07-12 12:30:10 +1000

    Update i3status for mpc

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-i3status -c ~/.i3/i3status.conf | (read line && echo $line && while :
+i3status -c ~/.i3/i3status.conf | (read line && echo $line && read line && echo $line && read line && echo $line && while :
 do
     read line
     dat=$(mpc current)
-    if [ -z "${dat}" ]; then
-        echo "$line" || exit 1
-    else
-        dat="[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" },"
-        echo "${dat} ${line#\[}" || exit 1
-    fi
+    echo ",[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" }, ${line#,\[}" || exit 1
 done)
