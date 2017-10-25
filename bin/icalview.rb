0c57a578ca9cf0bae7b68c9fcd854202317c27c8
Felix Hanley <felix@userspace.com.au>
2017-10-25 11:54:29 +1100

    Add ical viewer to mutt and mailcap

---
+++ bin/icalview.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require "rubygems" # apt-get install rubygems
+require "icalendar" # gem install icalendar
+require "date"
+
+class DateTime
+  def myformat
+    (self.offset == 0 ? self.strftime("%F %R") : self.strftime("%F %R %z"))
+  end
+end
+
+cals = Icalendar::Calendar.parse($<)
+cals.each do |cal|
+  cal.events.each do |event|
+    puts "Organizer: #{event.organizer}"
+    puts "Event:     #{event.summary}"
+    puts "Starts:    #{event.dtstart.myformat}"
+    puts "Ends:      #{event.dtend.myformat}"
+    puts "Location:  #{event.location}"
+    puts "Contact:   #{event.contact}"
+    puts "Description:\n#{event.description}"
+    puts ""
+  end
+end
