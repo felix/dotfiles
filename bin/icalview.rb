d6f47910994372b49785202a2eab170cad77f66e
Felix Hanley <felix@userspace.com.au>
2019-01-31 10:42:15 +1100

    Fix icalview

--- bin/icalview.rb
+++ bin/icalview.rb
@@ -2,21 +2,23 @@
 
 require "rubygems" # apt-get install rubygems
 require "icalendar" # gem install icalendar
-require "date"
+#require "date"
 
-class DateTime
-  def myformat
-    (self.offset == 0 ? self.strftime("%F %R") : self.strftime("%F %R %z"))
-  end
-end
+# class DateTime
+#   def myformat
+#     (self.offset == 0 ? self.strftime("%F %R") : self.strftime("%F %R %z"))
+#   end
+# end
 
 cals = Icalendar::Calendar.parse($<)
 cals.each do |cal|
   cal.events.each do |event|
     puts "Organizer: #{event.organizer}"
     puts "Event:     #{event.summary}"
-    puts "Starts:    #{event.dtstart.myformat}"
-    puts "Ends:      #{event.dtend.myformat}"
+    # puts "Starts:    #{event.dtstart.myformat}"
+    # puts "Ends:      #{event.dtend.myformat}"
+    puts "Starts:    #{event.dtstart}"
+    puts "Ends:      #{event.dtend}"
     puts "Location:  #{event.location}"
     puts "Contact:   #{event.contact}"
     puts "Description:\n#{event.description}"
