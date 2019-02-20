20796842fcc84b8d5c39076b0260f8b322447a79
Felix Hanley <felix@userspace.com.au>
2019-02-21 10:14:14 +1100

    Start using py3status

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -2,9 +2,10 @@
 
 hostname=$(hostname |cut -d. -f1)
 
-i3status -c ~/.i3/i3status-${hostname}.conf | (read line && echo $line && read line && echo $line && read line && echo $line && while :
-do
-    read line
-    dat=$(mpc current)
-    echo ",[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" }, ${line#,\[}" || exit 1
-done)
+#i3status -c ~/.i3/i3status-${hostname}.conf | (read line && echo $line && read line && echo $line && read line && echo $line && while :
+# do
+#     read line
+#     dat=$(mpc current)
+#     echo ",[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" }, ${line#,\[}" || exit 1
+# done)
+py3status -c ~/.i3/i3status-${hostname}.conf
