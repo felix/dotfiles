fa37b3fbbcaa9c01cd2d475e878bc986d2152f53
Felix Hanley <felix@userspace.com.au>
2019-10-12 17:26:07 +1100

    Try some tmux title changes

--- bin/common.sh
+++ bin/common.sh
@@ -28,6 +28,12 @@ dcu(){ docker-compose pull "$1" && docker-compose up -d "$1" && dcl "$1"; }
 
 calc(){ printf 'scale=2;%s\n' "$*" |bc; }
 
+ssh() {
+    tmux rename-window "$*"
+    command ssh "$@"
+    tmux rename-window "${SHELL/*\//}"
+}
+
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
   eval `ssh-agent`
   ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
