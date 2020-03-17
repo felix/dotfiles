2c9c4e58ead904fb3868cbf4e15ab31d53970495
Felix Hanley <felix@userspace.com.au>
2020-03-17 21:13:13 +1100

    common: remove ssh check

--- bin/common.sh
+++ bin/common.sh
@@ -39,7 +39,6 @@ if [ ! -S ~/.ssh/ssh_auth_sock ]; then
   eval `ssh-agent`
   ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
   ssh-add
-  [ -e "$HOME/.ssh/felix@home" ] && ssh-add $HOME/.ssh/felix@home
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 
