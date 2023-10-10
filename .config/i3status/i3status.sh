563af64e335a8f946c1a62621b9dd2f93cf32992
Felix Hanley <felix@userspace.com.au>
2023-10-11 10:52:26 +1100

    Some i3 environment updates

--- .config/i3status/i3status.sh
+++ .config/i3status/i3status.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-hostname=$(hostname |cut -d. -f1)
-
 #i3status -c ~/.i3/i3status-${hostname}.conf | (read line && echo $line && read line && echo $line && read line && echo $line && while :
 # do
 #     read line
 #     dat=$(mpc current)
 #     echo ",[{ \"color\": \"#859900\", \"full_text\": \"${dat}\" }, ${line#,\[}" || exit 1
 # done)
-$HOME/.local/bin/py3status -c ~/.config/i3status/${hostname}.conf
+$HOME/.local/bin/py3status -c ~/.config/i3status/i3status.conf
