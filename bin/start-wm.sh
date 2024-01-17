b7b09f805f449c260d5f9d1653cedc5784eb04aa
Felix Hanley <felix@userspace.com.au>
2024-01-17 15:44:28 +1100

    Update wm startup and dbus

--- bin/start-wm.sh
+++ bin/start-wm.sh
@@ -7,7 +7,7 @@ if [ -z "$XDG_RUNTIME_DIR" ]; then
 	chmod 700 "$XDG_RUNTIME_DIR"
 fi
 
-dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
+#dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
 
 [ -x "$(command -v alacritty)" ] && export TERMINAL=alacritty
 [ -x "$(command -v firefox)" ] && export BROWSER=firefox
@@ -15,8 +15,11 @@ dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
 
 export PASSWORD_STORE_X_SELECTION=primary
 export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland-egl
+export ELM_DISPLAY=wl
+export SDL_VIDEODRIVER=wayland
 
 # This will also be configured via .xinitrc
 #exec startx
 
-exec sway
+exec dbus-run-session sway
