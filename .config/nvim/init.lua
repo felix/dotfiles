cba6fa530c357d55cf67294d8bafc40f0567fbb7
Felix Hanley <felix@userspace.com.au>
2024-05-30 16:47:35 +1000

    vim: Restore lsp and snip configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,34 +10,71 @@ vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
 -- nvim-cmp setup
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local luasnip = require('luasnip')
 local cmp = require('cmp')
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 	mapping = {
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = function(fallback)
+		['<CR>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.select_next_item()
+				if luasnip.expandable() then
+					luasnip.expand()
+				else
+					cmp.confirm({
+						select = true,
+					})
+				end
 			else
 				fallback()
 			end
-		end,
-		['<S-Tab>'] = function(fallback)
+		end),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				if #cmp.get_entries() == 1 then
+					cmp.confirm({ select = true })
+				else
+					cmp.select_next_item()
+				end
+			elseif luasnip.locally_jumpable(1) then
+				luasnip.jump(1)
+			elseif has_words_before() then
+				cmp.complete()
+				if #cmp.get_entries() == 1 then
+					cmp.confirm({ select = true })
+				end
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end,
+		end, { "i", "s" }),
 	},
-	sources = {
+	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
+	}, {
 		{ name = 'buffer' },
-	},
+	})
 })
+
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
 
 -- Treesitter configuration
@@ -57,13 +94,13 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local servers = { 'gopls', 'tsserver', 'lua_ls' }
 local opts = { noremap=true, silent=true }
 local on_attach = function(client, buf)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(buf, 'n', '<leader>e', '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', opts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.api.nvim_buf_set_keymap(buf, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(buf, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(buf, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(buf, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(buf, 'n', '<leader>e', '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', opts)
 end
 for _, lsp in pairs(servers) do
 	require('lspconfig')[lsp].setup {
@@ -81,5 +118,8 @@ for _, lsp in pairs(servers) do
 				},
 			},
 		},
+		init_options = {
+			usePlaceholders = true,
+		}
 	}
 end
