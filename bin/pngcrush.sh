bf8bd7db9c0915515fe4c69b5e63ab094f03b3ce
Felix Hanley <felix@userspace.com.au>
2025-07-11 10:54:07 +1000

    Update pngcrush.sh to do all the things, even jpegs?!

--- bin/pngcrush.sh
+++ bin/pngcrush.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-for png in *.png; do
-    pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -brute -bail -reduce -ow $png
+OPTIPNG="$(command -v optipng)"
+ADVPNG="$(command -v advpng)"
+PNGCRUSH="$(command -v pngcrush)"
+JPEGOPTIM="$(command -v jpegoptim)"
+
+for f in *; do
+	printf '%s\n' "$f"
+	case ${f##*.} in
+		png)
+			[ -n "$OPTIPNG" ] && $OPTIPNG -quiet -force -o7 "$f"
+			[ -n "$ADVPNG" ] && $ADVPNG -z4 "$f"
+			[ -n "$PNGCRUSH" ] && $PNGCRUSH -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time -brute -reduce -q -q -ow "$f"
+			;;
+		jpg|jpeg)
+			[ -n "$JPEGOPTIM" ] && $JPEGOPTIM -f --strip-all "$f"
+			;;
+	esac
 done
